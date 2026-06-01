81165ccc79349c2d2ed00da6b7bf03872ee8121a
palak2344-png <palakm0809@gmail.com>
2026-06-01 20:59:31 +0545

    Add files via upload

---
+++ Day-1/sva3.c
@@ -0,0 +1,20 @@
+#Write a program to find factorial of a number.
+#include<stdio.h>
+int main()
+{
+    int n,i,fact;
+    fact=1;
+    printf("enter any number:");
+    scanf("%d",&n);
+    if (n<0)
+        printf("error");
+    else
+    {
+        for(i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        printf("factorial of %d is %d",n,fact);
+    }
+    return 0;
+}ss
