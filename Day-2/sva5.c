02d5f2f9ae8d0b429f1a4bc7ad8d5c8ac685c97a
palak2344-png <palakm0809@gmail.com>
2026-06-01 22:03:16 +0545

    Add files via upload

---
+++ Day-2/sva5.c
@@ -0,0 +1,15 @@
+#Write a program to find sum of digits of a number.
+#include<stdio.h>
+int main()
+{
+    int sum=0,n;
+    printf("enter a number:");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        sum=sum+n%10;
+        n=n/10;
+    }
+    printf("sum of digits:%d",sum);
+    return 0;
+}
