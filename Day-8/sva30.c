69a3ef2af1bf40dd391fb00cc87941d85ab4c242
palak2344-png <palakm0809@gmail.com>
2026-06-08 22:15:56 +0530

    Add files via upload

---
+++ Day-8/sva30.c
@@ -0,0 +1,15 @@
+//WAP to print number triangle.
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
