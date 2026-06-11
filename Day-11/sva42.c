b38052d8646a58aec7411a480a02352c03e3ba80
palak2344-png <palakm0809@gmail.com>
2026-06-11 22:52:31 +0530

    Add files via upload

---
+++ Day-11/sva42.c
@@ -0,0 +1,18 @@
+//Write a program to write function to find maximum.
+#include<stdio.h>
+
+int max(int a,int b)
+{
+    if(a>b)
+    return a;
+    else
+    return b;
+}
+int main()
+{
+    int x,y;
+    printf("Enter two numbers:");
+    scanf("%d%d",&x,&y);
+    printf("Maximum is %d",max(x,y));
+    return 0;
+}
