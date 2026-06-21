b78e94a4d815ac23576baff9449fe7702c93bfef
palak2344-png <palakm0809@gmail.com>
2026-06-21 09:15:40 +0530

    Add files via upload

---
+++ Day-22/sva86.c
@@ -0,0 +1,21 @@
+//Write a program to count words in a sentence.
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i, words = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if((i == 0 && str[i] != ' ' && str[i] != '\n') ||
+           (str[i] != ' ' && str[i - 1] == ' ')) {
+            words++;
+        }
+    }
+
+    printf("Number of words = %d\n", words);
+
+    return 0;
+}
