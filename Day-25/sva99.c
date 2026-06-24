9e9debdd7c97ef4ac8253b94457474aebde7e785
palak2344-png <palakm0809@gmail.com>
2026-06-24 21:39:46 +0530

    Add files via upload

---
+++ Day-25/sva99.c
@@ -0,0 +1,30 @@
+//Write a program to sort names alphabetically.
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char names[50][100], temp[100];
+    int n, i, j;
+    printf("Enter number of names: ");
+    scanf("%d", &n);
+    printf("Enter names:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%s", names[i]);
+    }
+
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(strcmp(names[i], names[j]) > 0) {
+                strcpy(temp, names[i]);
+                strcpy(names[i], names[j]);
+                strcpy(names[j], temp);
+            }
+        }
+    }
+    printf("\nNames in alphabetical order:\n");
+    for(i = 0; i < n; i++) {
+        printf("%s\n", names[i]);
+    }
+
+    return 0;
+}
